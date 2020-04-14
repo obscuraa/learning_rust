cbec60531867c3ec4c587b11f3e828923336bca5
JohnF <e229@mail.com>
2020-04-14 23:20:06 +0300

    test4 done

--- src/test4.rs
+++ src/test4.rs
@@ -5,11 +5,15 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    macro_rules! my_macro {
+		($val:expr) => {
+			std::format!("Hello {}", $val);
+		}
+    }
 
     #[test]
     fn test_my_macro_world() {
